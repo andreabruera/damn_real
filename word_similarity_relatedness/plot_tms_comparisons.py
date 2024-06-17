59da5280688eb6e1baf989cf30c394ad70a769a2
Andrea <andrea.bruera@gmail.com>
2024-06-17 15:20:39 +0100

    new plots

--- word_similarity_relatedness/plot_tms_comparisons.py
+++ word_similarity_relatedness/plot_tms_comparisons.py
@@ -78,18 +78,27 @@ for l, l_data in results.items():
     for t in rel_tasks:
         fig, ax = pyplot.subplots(constrained_layout=True, figsize=(20, 10))
         ax.set_ylim(bottom=0., top=0.3)
-        curr_ts = [w for w in l_data.keys() if t in w]
-        xs = [w.split('_tms_')[1] for w in l_data.keys() if t in w]
+        curr_ts = sorted([w for w in l_data.keys() if t in w])
+        xs = [w.split('_tms_')[1] for w in curr_ts]
         ax.bar(0, 0, color='mediumaquamarine', label='fasttext')
-        ax.bar(0, 0, color='goldenrod', label='')
+        ax.bar(0, 0, color='goldenrod', label='CC100-PPMI')
         ### results for fasttext
-        ft_model = lang_best[l][0]
+        #ft_model = lang_best[l][0]
+        ft_model = 'fasttext'
         ys = [l_data[c_t][ft_model] for c_t in curr_ts]
         ax.bar(
                [x-0.25 for x in range(len(xs))],
                [numpy.average(y) for y in ys],
                color='mediumaquamarine',
-               width=10,
+               width=0.4,
+               )
+        ax.scatter(
+               [x+0.25+(random.randint(-100, 100)*0.001) for x in range(len(xs)) for y in ys[x]],
+               ys,
+               edgecolor='aquamarine',
+               color='white',
+               alpha=0.5,
+               zorder=3.,
                )
         ### results for other model
         other_model = lang_best[l][1]
@@ -100,16 +109,17 @@ for l, l_data in results.items():
                [x+0.25 for x in range(len(xs))],
                [numpy.average(y) for y in ys],
                color='goldenrod',
-               width=10,
+               width=0.4,
                )
         ax.scatter(
-               [x+0.25+(random.randint(-100, 100)*0.001) for x in range(len(xs))],
-               [numpy.average(y) for y in ys],
+               [x+0.25+(random.randint(-100, 100)*0.001) for x in range(len(xs)) for y in ys[x]],
+               ys,
                edgecolor='goldenrod',
                color='white',
                alpha=0.5,
                zorder=3.,
                )
+        ax.legend(fontsize=20)
         pyplot.ylabel(
                       ylabel='RT-semantic dissimilarity correlation',
                       fontsize=23,
@@ -125,88 +135,5 @@ for l, l_data in results.items():
                      fontsize=30,
                      )
         pyplot.savefig(os.path.join('sim_rel', l, '{}.jpg'.format(t)))
-        pyplot.clf()
-        pyplot.close()
-
-
-import pdb; pdb.set_trace()
-
-for lang, l_res in results.items():
-    folder = os.path.join('sim_rel', lang)
-    os.makedirs(folder, exist_ok=True)
-    for task, t_res in l_res.items():
-        fig, ax = pyplot.subplots(
-                                  constrained_layout=True,
-                                  figsize=(20, 10),
-                                  )
-        if 'men' in task or '353' in task:
-            top = 0.85
-        elif 'simlex' in task:
-            top = 0.5
-        elif 'tms' in task:
-            top = 0.35
-        else:
-            top = 0.2
-        bottom = -.08 if task != 'men' else 0.1
-        ax.set_ylim(bottom=bottom, top=top)
-        all_vals = list()
-        fts = [k for k in t_res.keys() if 'fast' in k]
-        mitchs = [k for k in t_res.keys() if 'mitch' in k and 'rowincol' not in k]
-        #others = {k : sorted(vals.items(), key=lambda item : item[0]) for k, vals in t_res.items() if 'fast' not in k and 'mitch' not in k}
-        others = {k : sorted(vals.items(), key=lambda item : item[0]) for k, vals in t_res.items() if 'fast' not in k and 'mitch' not in k and 'top' in k and 'rowincol' not in k}
-        all_vals = [val[0] for v in others.values() for val in v] + [0.]
-        ax.hlines(
-                  y=0,
-                  xmin=-.1,
-                  xmax=max(all_vals)+.1,
-                  color='black',
-                  )
-        for ft, style in zip(fts, ['solid', 'dashdot',]):
-            ax.hlines(
-                      y=numpy.average(t_res[ft]),
-                      xmin=-.1,
-                      xmax=max(all_vals)+.1,
-                      label=ft,
-                      linestyles=style,
-                      color='red',
-                      )
-        for mitch, col in zip(mitchs, numpy.linspace(0, 1, len(mitchs))):
-            ax.hlines(
-                      y=numpy.average(t_res[mitch]),
-                      xmin=-.1,
-                      xmax=max(all_vals)+.1,
-                      label=mitch,
-                      color=matplotlib.colormaps['cividis'](col)
-                      )
-        colors = {k : v for k, v in zip(others.keys(), matplotlib.cm.rainbow(numpy.linspace(0, 1, len(others.keys()))))}
-        for case, sort_freqs in others.items():
-            xs_freqs = [v[0] for v in sort_freqs]
-            ys_freqs = [v[1] for v in sort_freqs]
-            ax.plot(
-                    xs_freqs,
-                    [numpy.average(v) for v in ys_freqs],
-                    label=case,
-                    color=colors[case]
-                    )
-            ax.scatter(
-                    xs_freqs,
-                    [numpy.average(v) for v in ys_freqs],
-                    s=50,
-                    marker='d',
-                    edgecolors='white',
-                    linewidths=1,
-                    color=colors[case],
-                    zorder=3
-                    )
-        pyplot.legend(
-                ncols=4,
-                loc=8,
-                fontsize=10,
-                )
-        pyplot.savefig(
-                        os.path.join(
-                                     folder,
-                             '{}.jpg'.format(task))
-                        )
         pyplot.clf()
         pyplot.close()
